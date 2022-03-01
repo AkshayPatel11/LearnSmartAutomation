0eaa7cd8867e48085822ba06ea77cc366c4d64f8
Akshay Patel <akshaypatel007@gmail.com>
2022-03-02 00:39:19 +0530

    Added Edit and verification method

--- LearnSmartAutomation/src/main/java/pages/Courses.java
+++ LearnSmartAutomation/src/main/java/pages/Courses.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
+import config.Configuration;
 import genericFunctions.CommonFunctions;
 import genericFunctions.VariableLibrary;
 
@@ -38,17 +40,35 @@ public class Courses extends BasePage {
 	By byButtonAddQuizzes = By.xpath("//div[@id='addCourseQuizListModal']//button[text()='Add']");
 
 	By bySaveButton = By.xpath("//button[@form='addCourseModal']");
+	By byEditButton = By.xpath("//div[@value='Edit']");
+	By byTextCourseSavedSuccesfully = By.xpath("//span[text()='Course saved successfully']");
 
-	public void fAddDetails() throws Exception {
-		courses = new Courses();
+	public void fAddDetails(Boolean createNew) throws Exception {
 		fwaitForpageLoad();
-		javaScriptClick(byTextAddButton);
-		waitForElement(byDropDownCoursetype, VariableLibrary.MEDIUM_WAIT);
-		selectByVisibleText(byDropDownCoursetype, "TEST SERIES");
-		type(byInputCourseName, oRecordset.getField("courseName"));
+
+		if (createNew) {
+			javaScriptClick(byTextAddButton);
+			waitForElement(byDropDownCoursetype, VariableLibrary.MEDIUM_WAIT);
+			selectByVisibleText(byDropDownCoursetype, "TEST SERIES");
+			type(byInputCourseName, oRecordset.getField("courseName"));
+		} else {
+			waitForElement(byInputCourseName, VariableLibrary.MEDIUM_WAIT);
+			// enter course name
+			clearInput(byInputCourseName);
+			type(byInputCourseName, oRecordset.getField("editCourseName"));
+		}
+
+		// enter description
+		clearInput(byInputDescription);
 		type(byInputDescription, oRecordset.getField("Description"));
+
+		// enter course access mode
 		selectByVisibleText(byDropDownCourseAccessMode, oRecordset.getField("courseAccessMode"));
+
+		// enter course content type
 		selectByVisibleText(byDropDownCourseContentType, oRecordset.getField("courseContentType"));
+
+		// enter course progression
 		selectByVisibleText(byDropDownCourseProgression, oRecordset.getField("courseProgression"));
 	}
 
@@ -85,23 +105,82 @@ public class Courses extends BasePage {
 		javaScriptClick(byButtonAddQuizzes);
 	}
 
+	public void fEditCourses() throws Exception {
+		driver.get(Configuration.getBaseURL() + "Course/Courses");
+		fwaitForpageLoad();
+		waitForElement(byTextAddButton, VariableLibrary.LONG_WAIT);
+		List<WebElement> courseList = driver.findElements(By.xpath("//table[contains(@class,'table-sm')]//tr//td[2]"));
+		int i = 0;
+		for (WebElement w : courseList) {
+			if (w.getText().trim().equalsIgnoreCase(oRecordset.getField("courseName"))) {
+				driver.findElements(byEditButton).get(i).click();
+				break;
+			}
+			i++;
+
+		}
+
+		fAddDetails(false);
+	}
+
+	public void fVerifyCourseCreatedOrUpdatedEntryInTable() throws Exception {
+		try {
+			fwaitForpageLoad();
+			waitForElement(byTextCourseSavedSuccesfully, VariableLibrary.LONG_WAIT);
+			// logStatus("info", "verifying course details in table");
+
+			isElementPresent(byTextCourseSavedSuccesfully, "Course not saved");
+
+			List<WebElement> courseList = driver
+					.findElements(By.xpath("//table[contains(@class,'table-sm')]//tr//td[2]"));
+			int listSize = courseList.size();
+
+			String sActualCourseName = driver
+					.findElement(By.xpath("//table[contains(@class,'table-sm')]//tr[" + listSize + "]//td[2]"))
+					.getText().trim();
+
+			String sAcutalDesc = driver
+					.findElement(By.xpath("//table[contains(@class,'table-sm')]//tr[" + listSize + "]//td[3]"))
+					.getText().trim();
+
+			Assert.assertEquals(sActualCourseName, oRecordset.getField("courseName"),
+					"CourseName is not Same actual courseName: " + sActualCourseName);
+
+			Assert.assertEquals(sAcutalDesc, oRecordset.getField("Description"),
+					"CourseName is not Same actual courseName: " + sAcutalDesc);
+
+			// logStatus("info", "verification of course entry in table completed");
+		} catch (Exception e) {
+			// logStatus("fail", e.getMessage());
+			// Assert.fail(e.getMessage());
+		} catch (AssertionError e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
 	public void fCourses(String sTestCaseId, String sSheetName) throws Exception {
 		CommonFunctions.fReadfromExcelSheet(sTestCaseId, sSheetName);
 
 		homePage.fSelectMainMenu(oRecordset.getField("mainMenu"));
 		homePage.fSelectSubMenu(oRecordset.getField("subMenu"));
+		boolean bNewFlag = false;
+		if (oRecordset.getField("createNewFlag").equalsIgnoreCase("1")) {
+			bNewFlag = true;
+			if (oRecordset.getField("detailsFlag").equalsIgnoreCase("1"))
+				fAddDetails(bNewFlag);
 
-		if (oRecordset.getField("detailsFlag").equalsIgnoreCase("1"))
-			fAddDetails();
+			if (oRecordset.getField("sectionsFlag").equalsIgnoreCase("1"))
+				fAddSections();
 
-		if (oRecordset.getField("sectionsFlag").equalsIgnoreCase("1"))
-			fAddSections();
-
-		if (oRecordset.getField("quizzesFlag").equalsIgnoreCase("1"))
-			fAddQuizzes();
-
+			if (oRecordset.getField("quizzesFlag").equalsIgnoreCase("1"))
+				fAddQuizzes();
+		} else if (oRecordset.getField("editFlag").equalsIgnoreCase("1")) {
+			fEditCourses();
+		}
 		// click on save button
 		javaScriptClick(bySaveButton);
+
+		fVerifyCourseCreatedOrUpdatedEntryInTable();
 	}
 
 }
